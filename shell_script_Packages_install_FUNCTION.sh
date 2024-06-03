1aa731b522e09f113ba18081dd15e9745ed107c6
Arunkumarpodili <121008660+Arunkumarpodili@users.noreply.github.com>
2024-06-03 23:24:12 +0530

    Create shell_script_Packages_install_FUNCTION.sh

---
+++ shell_script_Packages_install_FUNCTION.sh
@@ -0,0 +1,28 @@
+#!/bin/bash 
+
+
+UID=$(id -u)
+DATE=$(date +"%F-%H-%M")
+LOG_FILE=$DATE
+if [ $UID -ne 0 ]
+then
+        echo "User is not root"
+        exit 12
+else
+        echo "User is root"
+fi
+VALIDATE(){
+   if [ $1 -ne 0 ]
+   then
+       echo "$2 ----Failed"
+       exit 1
+   else
+       echo "$2 -----success"
+   fi
+}
+apt install git -y &>>$LOG_FILE
+VALIDATE $? "Git installation is"
+apt install wget -y &>>$LOG_FILE
+VALIDATE $? "wget installation is"
+apt install vim -y &>>$LOG_FILE
+VALIDATE $? "vim installation is"
